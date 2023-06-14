fc35c00b23ec0248b6278b647f310dc148cdac64
zoumana junior <jzoumana@gmail.com>
2023-06-14 15:07:56 +0000

    day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,20 +1,60 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("login page",
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
-        ),
-        ),
-      ),
-
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Bienvenue",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Entrer le nom d' utilisateur",
+                        labelText: "Non d' utilisateur"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Entrer le mot de passe utilisateur",
+                      labelText: "Mot de passe utilisateur",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 28.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Se connecter"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("pas de code");
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
